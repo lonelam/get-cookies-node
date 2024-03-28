4a7f850eee42d7a2a5f42aaa63aa42262231c569
Lai Zenan <z@laizn.com>
2024-03-29 01:38:15 +0800

    feat: format to prevent action errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 #![deny(clippy::all)]
 
+use get_cookies::read_cookie;
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
-use get_cookies::read_cookie;
 
 #[napi]
 pub async fn get_cookies(input: String) -> Result<String> {
-    read_cookie(&input).await.map_err(|e| {
-        // Convert your error to `napi::Error` here
-        napi::Error::new(napi::Status::GenericFailure, format!("Error reading cookie: {}", e))
-    })
+  read_cookie(&input).await.map_err(|e| {
+    // Convert your error to `napi::Error` here
+    napi::Error::new(
+      napi::Status::GenericFailure,
+      format!("Error reading cookie: {}", e),
+    )
+  })
 }
