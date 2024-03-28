c603d657d79c0822dce9fec1243ce722109d35ba
Lai Zenan <z@laizn.com>
2024-03-29 01:46:47 +0800

    feat: move get-cookies to windows build temporary

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![deny(clippy::all)]
 
-use get_cookies::read_cookie;
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
 
 #[cfg(target_os="windows")]
 #[napi]
 pub async fn get_cookies(input: String) -> Result<String> {
+  use get_cookies::read_cookie;
   read_cookie(&input).await.map_err(|e| {
     // Convert your error to `napi::Error` here
     napi::Error::new(
